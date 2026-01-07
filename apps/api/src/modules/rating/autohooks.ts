189cf0ba37bb241993cfb4f40f49e4acf905609a
Vo Quang Chien <voquangchien.dev@proton.me>
2026-01-07 15:18:07 +0700

    chore: clean up

--- apps/api/src/modules/rating/autohooks.ts
+++
@@ -1,5 +0,0 @@
-import { addRouteTags } from '@/hooks/onRoute';
-
-export default function ratingHooks(fastify: FastifyTypeBox) {
-  fastify.addHook('onRoute', addRouteTags('Rating'));
-}
--- apps/web/lib/constants.ts
+++
@@ -1,7 +0,0 @@
-export const API_BASE_URL = () => {
-  if (typeof window !== 'undefined') {
-    return process.env.NEXT_PUBLIC_API_URL;
-  }
-
-  return process.env.API_URL;
-};
