b141054efa1c3034458ae09d220242714b69e8b3
Vo Quang Chien <2giosangmitom@proton.me>
2026-01-12 13:41:47 +0700

    feat(shared): add enums to shared

---
+++ packages/shared/src/enums.ts
@@ -0,0 +1,24 @@
+export enum AccountStatus {
+  ACTIVE = "ACTIVE",
+  BANNED = "BANNED",
+}
+
+export enum BookCondition {
+  NEW = "NEW",
+  GOOD = "GOOD",
+  WORN = "WORN",
+  DAMAGED = "DAMAGED",
+  LOST = "LOST",
+}
+
+export enum LoanStatus {
+  BORROWED = "BORROWED",
+  RETURNED = "RETURNED",
+  OVERDUE = "OVERDUE",
+}
+
+export enum Role {
+  ADMIN = "ADMIN",
+  LIBRARIAN = "LIBRARIAN",
+  MEMBER = "MEMBER",
+}
