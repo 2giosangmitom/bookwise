136db9d060140928753eaa43e360313a1ffed70c
Vo Quang Chien <2giosangmitom@proton.me>
2026-01-13 16:01:48 +0700

    fix(api): don't use `@Response`

--- apps/api/src/modules/account/account.controller.ts
+++ apps/api/src/modules/account/account.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post, Response, UsePipes } from "@nestjs/common";
+import { Body, Controller, Post, UsePipes } from "@nestjs/common";
 import { AccountService } from "./account.service";
 import { signUpResponseSchema, signUpSchema, type signUpDTO } from "@bookwise/shared";
 import { ZodValidationPipe } from "@/pipes/zod";
@@ -6,7 +6,6 @@ import { SignUpResponse } from "@bookwise/shared";
 import { ApiBody, ApiCreatedResponse } from "@nestjs/swagger";
 import { SchemaObject } from "@/types";
 import z from "zod";
-import { type FastifyReply } from "fastify";
 
 @Controller("/account")
 export class AccountController {
@@ -17,14 +16,15 @@ export class AccountController {
   @ApiBody({
     schema: z.toJSONSchema(signUpSchema) as SchemaObject,
   })
-  @ApiCreatedResponse({ schema: z.toJSONSchema(signUpResponseSchema) as SchemaObject })
-  async signUp(@Body() body: signUpDTO, @Response() res: FastifyReply): Promise<SignUpResponse> {
+  @ApiCreatedResponse({
+    schema: z.toJSONSchema(signUpResponseSchema) as SchemaObject,
+    description: "Account has been created successfully",
+  })
+  async signUp(@Body() body: signUpDTO): Promise<SignUpResponse> {
     const createdAccount = await this.accountServive.signUp(body);
 
-    res.code(201);
-
     return {
-      message: "Account created successfully",
+      message: "Account has been created successfully",
       data: { userId: createdAccount.user.id },
     };
   }
