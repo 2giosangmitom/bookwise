0322b23234370a6187e3c02adb1c46ffddbd5863
Vo Quang Chien <2giosangmitom@proton.me>
2026-01-13 16:23:12 +0700

    refactor: update types and swagger schema

--- apps/api/src/filters/zod.ts
+++ apps/api/src/filters/zod.ts
@@ -4,8 +4,8 @@ import { ZodError } from "zod";
 @Catch(ZodError)
 export class ZodExceptionFilter implements ExceptionFilter {
   catch(exception: ZodError) {
-    throw new BadRequestException(
-      exception.issues.map((issue) => `${issue.path.join(", ")}: ${issue.message}`).join(" / "),
-    );
+    const errors = exception.issues.map((issue) => `${issue.path.join(", ")}: ${issue.message}`).join(" / ");
+
+    throw new BadRequestException(errors);
   }
 }
