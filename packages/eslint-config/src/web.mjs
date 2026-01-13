c5e92742483fc94d596113eb6456d47335ce5c51
Vo Quang Chien <2giosangmitom@proton.me>
2026-01-13 20:48:38 +0700

    feat(web): update components

--- packages/eslint-config/src/web.mjs
+++ packages/eslint-config/src/web.mjs
@@ -3,4 +3,8 @@ import baseConfig from "./base.mjs";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
 
-export default defineConfig(...baseConfig, ...nextVitals, ...nextTs);
+export default defineConfig(...baseConfig, ...nextVitals, ...nextTs, {
+  rules: {
+    "react-hooks/purity": "off",
+  },
+});
