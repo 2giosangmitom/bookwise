ae8b6d4098c35dc4da5571db025b11f52d3987f1
Vo Quang Chien <2giosangmitom@proton.me>
2026-01-14 15:12:31 +0700

    fix(api): don't throw error in zod pipe

--- apps/api/src/pipes/zod.ts
+++ apps/api/src/pipes/zod.ts
@@ -1,15 +1,11 @@
-import { PipeTransform, BadRequestException } from "@nestjs/common";
+import { PipeTransform } from "@nestjs/common";
 import { z } from "zod";
 
 export class ZodValidationPipe implements PipeTransform {
   constructor(private schema: z.ZodType) {}
 
   transform(value: unknown) {
-    try {
-      const parsedValue = this.schema.parse(value);
-      return parsedValue;
-    } catch (error) {
-      throw new BadRequestException(error);
-    }
+    const parsedValue = this.schema.parse(value);
+    return parsedValue;
   }
 }
