bdf79b73358e411298d1ed384261c34fa89468a2
Vo Quang Chien <2giosangmitom@proton.me>
2026-01-15 16:44:05 +0700

    chore(api): update test suite name

--- apps/api/src/utils/__tests__/hashing.test.ts
+++ apps/api/src/utils/__tests__/hashing.test.ts
@@ -1,7 +1,7 @@
 import { describe, it, expect } from "@jest/globals";
 import { HashingUtils } from "../hashing";
 
-describe("utils.hashing", () => {
+describe("HashingUtils", () => {
   const hashingUtils = new HashingUtils();
 
   describe("generateHash", () => {
